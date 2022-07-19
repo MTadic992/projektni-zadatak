46281b5eb60eb6182fdc181c2d33db4386b35974
Balotelli-45 <mariovk92@gmail.com>
2022-07-19 16:12:23 +0200

    projektni zadatak-novo

--- src/pages/JoinPage/JoinPage.component.jsx
+++ src/pages/JoinPage/JoinPage.component.jsx
@@ -1,15 +1,22 @@
 import "./JoinPage.styles.scss";
 
 import { JoinForm } from "../../components/JoinForm";
+import { useUser } from "../../contexts/UserContext";
 
 export function JoinPage(props) {
+  const { setUser } = useUser();
+
+  const joinWithUser = (formState) => {
+    setUser({ displayName: formState.displayName });
+  }
+
   return (
     <div className="join-page">
       <div className="join-page__title">
         Chat with friends
       </div>
       <div className="join-page__form">
-        <JoinForm />
+        <JoinForm onJoin={joinWithUser} />
       </div>
     </div>
   );
